0746cd1b5148c3f613580f4b37165e2a2fe162f9
Luís Cipriani <cunhaluisg5@gmail.com>
2024-02-06 21:53:13 -0300

    Finaliza projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>Minhas Tarefas</header>
+    <TaskComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TaskComponent from './components/Task.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    TaskComponent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  header{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    font-size: 40px;
+    color: #FFF;
+  }
 </style>
